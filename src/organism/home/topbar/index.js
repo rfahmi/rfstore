c7d834b61b4b44d2a8c168ab19f32fe4ddb9017c
rfahmi <rfahmi@outlook.com>
2020-12-11 02:26:48 +0700

    play with animation

--- src/organism/home/topbar/index.js
+++ src/organism/home/topbar/index.js
@@ -1,50 +1,69 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {memo} from 'react';
+import React, {memo, useEffect, useState} from 'react';
 import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
 import {IconButton} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {colors} from '../../../constants/colors';
+import FocusAwareStatusBar from '../../../components/FocusAwareStatusBar';
 
-const HomeTopBar = () => {
+const HomeTopBar = ({headerOpacity}) => {
   const navigation = useNavigation();
+  const [opacity, setOpacity] = useState(0);
+  useEffect(() => {
+    if (!headerOpacity) {
+      return;
+    }
+    const listenerId = headerOpacity.addListener((a) => {
+      setOpacity(a.value);
+    });
+    return () => headerOpacity.removeListener(listenerId);
+  });
   return (
-    <View
-      style={{
-        marginTop: StatusBar.currentHeight,
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        paddingHorizontal: 8,
-        zIndex: 1,
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-        flexDirection: 'row',
-      }}>
-      <TouchableOpacity
-        style={{
-          backgroundColor: '#fff',
-          borderRadius: 8,
-          padding: 8,
-          flex: 1,
-        }}
-        onPress={() => navigation.navigate('Search')}>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Icon color="#999" size={14} name="magnify" />
-          <Text style={{fontSize: 14, color: '#999', marginLeft: 5}}>
-            Cari produk
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <IconButton
-        icon="bell"
-        color={colors.white}
-        size={24}
-        style={{zIndex: 1}}
-        onPress={() => console.log('Pressed')}
+    <>
+      <FocusAwareStatusBar
+        translucent
+        barStyle={0 < 0.5 ? 'light-content' : 'dark-content'}
+        backgroundColor={`rgba(200,200,200,${opacity})`}
       />
-    </View>
+      <View
+        style={{
+          marginTop: StatusBar.currentHeight,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          paddingHorizontal: 8,
+          zIndex: 1,
+          flex: 1,
+          alignItems: 'center',
+          backgroundColor: `rgba(255,255,255,${opacity})`,
+          flexDirection: 'row',
+        }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#fff',
+            borderRadius: 8,
+            padding: 8,
+            flex: 1,
+            elevation: opacity,
+          }}
+          onPress={() => navigation.navigate('Search')}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Icon color="#999" size={14} name="magnify" />
+            <Text style={{fontSize: 14, color: '#999', marginLeft: 5}}>
+              Cari produk
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <IconButton
+          icon="bell"
+          color={opacity < 0.5 ? colors.white : colors.grayDark}
+          size={24}
+          style={{zIndex: 1}}
+          onPress={() => console.log('Pressed')}
+        />
+      </View>
+    </>
   );
 };
 
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import {FlatList, View} from 'react-native';
-import FocusAwareStatusBar from '../../components/FocusAwareStatusBar';
+import React, {useState, useRef, useEffect} from 'react';
+import {View, Animated, FlatList} from 'react-native';
 import Banners from '../../organism/home/banners';
 import Categories from '../../organism/home/categories';
 import Panels from '../../organism/home/panels';
@@ -11,11 +10,26 @@ const Home = () => {
   const categories = require('../../dummy/categories.json');
   const panels = require('../../dummy/panels.json');
 
+  const headerOpacity = useRef(new Animated.Value(0)).current;
+
+  const _handleScroll = (event) => {
+    const scrollPosition = event.nativeEvent.contentOffset.y;
+    if (scrollPosition < 200) {
+      Animated.timing(headerOpacity, {
+        toValue: 0,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(headerOpacity, {
+        toValue: 1,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
   const _renderTop = () => {
     return (
       <View style={{alignItems: 'center'}}>
-        <FocusAwareStatusBar translucent backgroundColor="transparent" />
-        <TopBar />
         <Banners banners={banners} />
         <Categories categories={categories} />
       </View>
@@ -30,15 +44,20 @@ const Home = () => {
   };
 
   return (
-    <FlatList
-      data={panels}
-      ListHeaderComponent={_renderTop}
-      renderItem={_renderPanel}
-      keyExtractor={keyExtractor}
-      removeClippedSubviews
-      initialNumToRender={1}
-      maxToRenderPerBatch={1}
-    />
+    <>
+      <TopBar headerOpacity={headerOpacity} />
+      <FlatList
+        onScroll={_handleScroll}
+        scrollEventThrottle={16}
+        data={panels}
+        ListHeaderComponent={_renderTop}
+        renderItem={_renderPanel}
+        keyExtractor={keyExtractor}
+        removeClippedSubviews
+        initialNumToRender={1}
+        maxToRenderPerBatch={1}
+      />
+    </>
   );
 };
 
